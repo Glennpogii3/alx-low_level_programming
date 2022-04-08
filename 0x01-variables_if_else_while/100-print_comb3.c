a74052295056561653363ce4ca9bbbbf90c97fd7
Glennpogii3 <44199266+Glennpogii3@users.noreply.github.com>
2022-04-09 00:50:47 +0800

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,14 +16,13 @@ int main(void)
 		for (m = 49 ; m <= 57; m++)
 		{
 			if (m > n)
-			{		
-					putchar(n);
-					putchar(m);
+			{
+				putchar(n);
+			 putchar(m);
 			if (n != 56 || m != 57)
 			{
 				putchar(',');
 				putchar(' ');
-				
 				}
 			}
 		}
