9fd85859b3e7ca5c2d797e6c4a8f8896472d6626
Glennpogii3 <okafor21boo@gmail.com>
2022-04-14 15:26:51 +0800

    0x03 debuggin

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - Lanzador
+*
+* Return: Always 0
+*
+*/
+
+int main(void)
+{
+        int i;
+
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
+}
